e7ff3e78d98bbadb2495164fb642c1b5fea4429d
Mirko Boruc <mkbu@demant.com>
2021-11-08 20:03:53 +0100

    second usage of useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //console.log(useState("hello world"));
+
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+
+  // console.log(value);
+  // console.log(handler);
+  const [text, setText] = useState("random title");
+  const clickHandler = () => {
+    if (text === "random title") {
+      setText("new title");
+    } else if (text === "new title") {
+      setText("kamil miszcz");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" onClick={clickHandler}>
+        change title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
